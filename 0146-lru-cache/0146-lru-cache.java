53e924c59b156f255222e233cac30ef2caab6ba4
Sakshi Oswal <oswalsakshi17@gmail.com>
2023-07-29 01:35:44 +0530

    Time: 37 ms (100%), Space: 120.8 MB (43.14%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,114 +1,82 @@
 class LRUCache {
-    Node head = null;
-    Node tail = null;
+    Node head = new Node(0,0);
+    Node tail = new Node(0,0);
     int capacity;
     HashMap<Integer,Node> map = new HashMap();
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;     
     }
     
     public int get(int key) {
         if(map.containsKey(key))
         {
-            Node n = map.get(key);
-            // tail is not pointing to curr key
-            if(tail.key != key)
-            {
-                n.delete();
-                tail.insert(n);
-            }
+            Node n = map.get(key);                       
+            n.delete();
+            head.insert(n); 
+            // System.out.println(map+"head.n.key : "+head.next.key
+            // +" : tail.p.key "+tail.prev.key);   
             return n.val;
         }
         return -1;
     }
-    
+
     public void put(int key, int value) {
         if(map.containsKey(key))
         {
             Node n = map.get(key);
-            n.setVal(value);
-            // tail is not pointing to curr key
-            if(tail.key != key)
-            {
-                n.delete();
-                tail.insert(n);
-
-            }
+            n.delete();
+            head.insert(n);
+            n.setVal(value);            
         }
         else
         {
             if(map.size() >= capacity)
             {
-                //remove head element (LRU)
-                map.remove(head.key);
-                head.delete();
+                //remove tail's prev element (LRU)
+                map.remove(tail.prev.key);
+                tail.prev.delete();
             }
             //insert new element
-            Node n = new Node(key,value,tail,null);
-            if(head == null)
-            {
-                head = n;
-            }
-            if(tail == null)
-            {
-                tail = n;
-            }
-            // tail is not pointing to curr key
-            if(tail.key != key)
-            {
-                n.delete();
-                tail.insert(n);
-            }
-            map.put(key,n);           
+            Node n = new Node(key,value);            
+            head.insert(n);         
+            map.put(key,n);   
+            // System.out.println(map+"head.n.key : "+head.next.key
+            // +" : tail.p.key "+tail.prev.key);        
         }
     }
 
-    class Node{
+    public class Node{
         Node next;
         Node prev;
         int val;
         int key;
-        Node(int key,int val,Node prev,Node next)
+        Node(int key,int val)
         {
             this.val = val;
-            this.next = next;
-            this.prev = prev;
-            this.key = key;
+            this.key = key; 
         }
         void insert(Node n)
-        {
-            next = n;
+        {         
             n.prev = this;
-            n.next = null;
-            if(tail == this)
-            {
-                tail = n;
-            }
+            n.next = next;
+            next.prev = n; 
+            next = n;           
         }
         void delete()
-        {
-            if(prev != null)
-            {
-                prev.next = next;
-            }
-            //Its a head
-            else
-            {
-                head = next;
-            }
-            if(next != null)
-            {
-                next.prev = prev;
-            }
-            //its a tail
-            else
-            {
-                tail = prev;
-            }
+        {         
+            prev.next = next;       
+            next.prev = prev;           
         }
         void setVal(int val)
         {
             this.val = val;
+        }
+        @Override
+        public String toString()
+        {
+            return ""+key+":"+val;
         }
     }
 }
