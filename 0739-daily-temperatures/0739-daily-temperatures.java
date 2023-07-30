3e78d7474df2946d8af6ae287c5b5cbf3779b729
Sakshi Oswal <oswalsakshi17@gmail.com>
2023-07-30 15:54:40 +0530

    Time: 75 ms (77.64%), Space: 59 MB (17.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,19 +1,24 @@
 class Solution {
+//     Monotonic Stack
+// Whenever a problem requires enumerating values for indices on the basis of other values in an array, one should think of including a stack in the solution! And that too, a monotonic stack. It is simply a stack which contains values in a monotonic order; i.e either increasing or decreasing.
+// Imagine there is a data structure which contains the indices whose temperatures are in an increasing order. Then, enumerating the next index which has a greater temperature will be easy: we simply pop the elements till the top element has a greater temperature than the current index!
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Pair> s= new Stack();
-        s.push(new Pair(temperatures[0],0));
         int[] result = new int[temperatures.length];
-        for(int i = 0;i<result.length;i++)
+        for(int i = temperatures.length-1;i>=0;i--)
         {
-            result[i] = 0;
-        }
-        for(int i = 1;i<temperatures.length;i++)
-        {
-            while(!s.isEmpty() && temperatures[i]>s.peek().val)
+            //pop until current ele is greater than s.top()
+            //maintaining a decreasing monotonic stack
+            // i.e botton has largest and top has smallest
+            while(!s.isEmpty() && temperatures[i] >= s.peek().val)
             {
-                int index = s.peek().index;
                 s.pop();
-                result[index] = i-index;
+            }
+            // if stack contains anything i.e the next greater element
+            if(!s.isEmpty())
+            {            
+                int index = s.peek().index;
+                result[i] = index-i;                   
             }
             s.push(new Pair(temperatures[i],i));
         }
