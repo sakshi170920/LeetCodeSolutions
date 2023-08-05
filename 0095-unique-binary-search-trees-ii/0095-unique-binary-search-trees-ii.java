5d4a72ac8453ff0f600315776ba68f08321a8e47
Sakshi Oswal <oswalsakshi17@gmail.com>
2023-08-05 16:36:29 +0530

    Time: 1 ms (99.88%), Space: 43.6 MB (66.87%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -14,126 +14,31 @@
  * }
  */
 class Solution {
-    public void  flat(TreeNode root,List<Integer> result)
+    public List<TreeNode> allPossibleBST(int start,int end)
     {
-        if(root == null)
+        List<TreeNode> result= new ArrayList();
+        if(start > end)
         {
             result.add(null);
-            return;
+            return result;
         }
-        result.add(root.val);
-        flat(root.left,result);
-        flat(root.right,result);
-    }
-    public void helper(int n,TreeNode root,boolean[] visited,List<TreeNode> result,
-    List<List<Integer>> res)
-    {
-        boolean flag = true;
-        for(int i = 0;i<visited.length;i++)
+        for(int i = start;i<= end;i++)
         {
-            if(!visited[i])
+            List<TreeNode> lefts = allPossibleBST(start,i-1);
+            List<TreeNode> rights = allPossibleBST(i+1,end);
+            for(TreeNode left : lefts)
             {
-                visited[i] = true;
-                root = insertNode(root,i+1);
-                helper(n,root,visited,result,res);
-                visited[i] = false;
-                root = deleteNode(root,i+1);
-                flag = false;
+                for(TreeNode right : rights)
+                {
+                    TreeNode root = new TreeNode(i,left,right);
+                    result.add(root);
+                }
             }
         }
-        if(flag == true && root!= null)
-        {
-            //System.out.println("added in result with root : "+root.val); 
-            List<Integer> prev = new ArrayList();
-            // if(!result.isEmpty())
-            // {
-            // flat(result.get(result.size()-1),prev);
-            // List<Integer> curr = new ArrayList();
-            // flat(root,curr);
-            // if(prev.equals(curr))
-            //     return;
-            // }
-            List<Integer> curr = new ArrayList();
-            flat(root,curr);
-            if(res.contains(curr))
-                return;
-            res.add(curr);
-            TreeNode temp = clone(root);   
-            result.add(temp);
-        }
-    }
-    public TreeNode clone(TreeNode root)
-    {
-        if(root == null)
-        return root;
-        TreeNode clone = new TreeNode(root.val);
-        clone.left = clone(root.left);
-        clone.right = clone(root.right);
-        return clone;
-    }
-    public TreeNode insertNode(TreeNode root,int key)
-    {
-        if(root == null)
-        {
-            //System.out.println("inserted : "+key);      
-            root = new TreeNode(key);
-            return root;
-        }
-        if(key == root.val)
-        {
-            return root;
-        }
-        if(key < root.val)
-        {
-            root.left = insertNode(root.left,key);
-        }
-        else
-        {
-            root.right = insertNode(root.right,key);         
-        }
-        return root;
-    }
-    public TreeNode smallest(TreeNode root)
-    {
-        while(root.left != null)
-        {
-            root = root.left;
-        }
-        return root;
-    }
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null)
-            return root;
-        if(root.val > key)
-        {
-            root.left = deleteNode(root.left,key);
-        }
-        else if(root.val < key)
-        {
-            root.right = deleteNode(root.right,key);
-        }
-        else
-        {     
-            //System.out.println("found : "+root.val);      
-            //found
-            if(root.left == null && root.right == null)
-                return null;
-            if(root.right == null)
-            {
-                return root.left;
-            }
-            TreeNode smallest = smallest(root.right);
-            root.val = smallest.val;
-            // left will stay same
-            root.right = deleteNode(root.right,smallest.val);
-        }
-        return root;
+        return result;
     }
     public List<TreeNode> generateTrees(int n) {
-        boolean[] visited = new boolean[n];
         List<TreeNode> result = new ArrayList();
-        List<List<Integer>> res = new ArrayList();
-        helper(n,null,visited,result,res);
-        return result;
+        return allPossibleBST(1,n);
     }
 }
