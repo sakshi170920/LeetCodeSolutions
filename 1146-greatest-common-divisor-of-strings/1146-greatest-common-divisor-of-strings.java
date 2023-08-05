1595143824a7f63a12ae15f34ccdafa913b6f242
Sakshi Oswal <oswalsakshi17@gmail.com>
2023-08-05 19:40:09 +0530

    Time: 2 ms (22.72%), Space: 41.5 MB (34.62%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,63 +1,22 @@
 class Solution {
+    int len1=0,len2 = 0;
+    public boolean valid(String s1,String s2,int k)
+    {
+        if(len1%k > 0 || len2%k > 0)
+            return false;
+        String base = s1.substring(0,k);
+        return s1.replace(base,"").isEmpty() && s2.replace(base,"").isEmpty();
+    }
     public String gcdOfStrings(String str1, String str2) {
-        StringBuilder result = new StringBuilder(str1.length()<str2.length() ? str1 : str2);
-        String temp = result.toString();
-        temp =  temp.equals(str1) ? str2 : str1;
-        System.out.println(" result : "+result+" TEMP : "+temp);
-        for(int i = 0;i<temp.length();i++)
+        len1=str1.length();
+        len2=str1.length();
+        for(int i = Math.min(len1,len2);i>=1;i--)
         {
-            if(result.isEmpty())
+            if(valid(str1,str2,i))
             {
-                return result.toString();
+                return str1.substring(0,i);
             }
-            //System.out.println(" i : "+i+" result : "+result);
-            int j = 0;
-            int k = i;
-            for(;j<result.length() && k<temp.length();j++)
-            {
-                if(result.charAt(j) != temp.charAt(k))
-                    break;
-                k++;
-            }
-            if(j<result.length())
-            {
-                //System.out.println(" not found going back to : "+j);
-                result.setLength(j);
-                i=j-1;
-            }
-            else
-            {
-                i+=j-1;
-            }            
         }
-        temp =  temp.equals(str1) ? str2 : str1;
-        System.out.println(" result : "+result+" TEMP : "+temp);
-        for(int i = 0;i<temp.length();i++)
-        {
-            if(result.isEmpty())
-            {
-                return result.toString();
-            }
-            //System.out.println(" i : "+i+" result : "+result);
-            int j = 0;
-            int k = i;
-            for(;j<result.length() && k<temp.length();j++)
-            {
-                if(result.charAt(j) != temp.charAt(k))
-                    break;
-                k++;
-            }
-            if(j<result.length())
-            {
-                //System.out.println(" not found going back to : "+j);
-                result.setLength(j);
-                i=j-1;
-            }
-            else
-            {
-                i+=j-1;
-            }            
-        }
-        return result.toString();
+        return "";
     }
 }
