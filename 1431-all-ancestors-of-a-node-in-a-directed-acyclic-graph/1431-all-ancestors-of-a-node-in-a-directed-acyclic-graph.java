1636ce8d14c17efc19f7a411529492f2b1b45c9a
Sakshi Oswal <oswalsakshi17@gmail.com>
2023-08-12 12:10:56 +0530

    Time: 50 ms (96.88%), Space: 64.3 MB (85.84%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,54 +1,37 @@
 class Solution {
+    public void dfs(int src,int ancestor,List<List<Integer>> graph,boolean[] visited,List<List<Integer>> result){
+        visited[src] = true;
+        for(Integer child : graph.get(src))
+        {
+            if(!visited[child])
+            {
+                result.get(child).add(ancestor);
+                dfs(child,ancestor,graph,visited,result);
+            }
+        }
+    }
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        List<Set<Integer>> result = new ArrayList(n);
+        List<List<Integer>> result = new ArrayList();
+        List<List<Integer>> graph = new ArrayList();
         for(int i = 0;i<n;i++)
         {
-            result.add(new HashSet());
-        }
-        int[] indegree = new int[n];
-        for(int[] edge : edges)
-        {
-            indegree[edge[1]]++;
-        }
-        List<List<Integer>> graph = new ArrayList(n);
-        for(int i = 0;i<n;i++)
-        {
+            result.add(new ArrayList());
             graph.add(new ArrayList());
         }
         for(int[] edge : edges)
         {
             graph.get(edge[0]).add(edge[1]);
         }
-        Queue<Integer> q = new LinkedList();
         for(int i = 0;i<n;i++)
         {
-            if(indegree[i] == 0)
-            {
-                q.add(i);
-            }
+            boolean[] visited = new boolean[n];
+            visited[i] = true;
+            //start with src = 0 and traverse all children of 0 and add it as ancesstor
+            // then start with 1 
+            // do it for all nodes 1 by 1 so order is maintained, however more traversals
+            dfs(i,i,graph,visited,result);
+            
         }
-        while(!q.isEmpty())
-        {
-            int node = q.poll();
-            for(int neighbour : graph.get(node))
-            {
-                result.get(neighbour).addAll(result.get(node));
-                result.get(neighbour).add(node);
-                indegree[neighbour]--;
-                if(indegree[neighbour] == 0)
-                {
-                    q.add(neighbour);
-                }
-            }
-        }
-        List<List<Integer>> res = new ArrayList(n);
-        for(int i = 0;i<n;i++)
-        {
-            res.add(new ArrayList());
-            List<Integer> temp = new ArrayList(result.get(i));
-            Collections.sort(temp);
-            res.get(i).addAll(temp);
-        }
-        return res;
+        return result;
     }
 }
