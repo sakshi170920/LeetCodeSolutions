3b1280217e7794cbf42e4cc920ca54e628650636
Sakshi Oswal <oswalsakshi17@gmail.com>
2023-08-23 20:50:59 +0530

    Time: 11 ms (7.54%), Space: 43.4 MB (8.85%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String reorganizeString(String s) {
+        char[] res = new char[s.length()];
+        Arrays.fill(res,' ');
+        //System.out.println(arr[0]);
+        HashMap<Character,Integer> map = new HashMap();
+        for(int i = 0;i<s.length(); i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        PriorityQueue<Freq> q = new PriorityQueue<Freq>((a,b)->b.c-a.c);
+        for(Map.Entry<Character,Integer> a : map.entrySet()){
+            q.add(new Freq(a.getKey(),a.getValue()));
+        }
+        String result = "";
+        if(Math.ceil(s.length()/2.0)<q.peek().c)
+            return "";
+        while(!q.isEmpty()){
+            Freq curr1 = q.poll();
+            Freq curr2 = null;
+            if(!q.isEmpty())
+                curr2 = q.poll();
+            if(result.isEmpty() || result.charAt(result.length()-1) 
+            != curr1.a){
+                result+=curr1.a;
+                if(curr1.c > 1)
+                    q.add(new Freq(curr1.a,curr1.c-1));
+                curr1 = null;
+            }
+            if(curr2 != null){
+                result+=curr2.a;
+                if(curr2.c > 1)
+                    q.add(new Freq(curr2.a,curr2.c-1));
+            }
+            if(curr1!= null){
+                result+=curr1.a;
+                if(curr1.c > 1)
+                    q.add(new Freq(curr1.a,curr1.c-1));
+            } 
+        }
+        return result;
+    }
+    class Freq{
+        char a;
+        int c;
+        Freq(char a,int c){
+            this.a = a;
+            this.c = c;
+        }
+    }
+}
