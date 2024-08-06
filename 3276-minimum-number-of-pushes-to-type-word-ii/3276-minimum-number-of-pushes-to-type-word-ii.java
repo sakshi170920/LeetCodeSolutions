760c9a81b2113c9989f434a1a17b0d82928952aa
Sakshi Oswal <oswalsakshi17@gmail.com>
2024-08-06 23:47:55 +0530

    Time: 20 ms (34.81%), Space: 45.7 MB (45.19%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,24 +1,24 @@
 class Solution {
+    int C = 26;
     public int minimumPushes(String word) {
-        HashMap<Character,Integer> freq = new HashMap();
+        Integer[] freq = new Integer[C];
+        for(int i = 0;i<C;i++){
+            freq[i] = 0;
+        }
         for(int i = 0;i<word.length();i++){
             char c = word.charAt(i);
-            freq.put(c,freq.getOrDefault(c,0)+1);
+            freq[c-'a']++;
         }
 
-        List<Integer> list
-            = freq.entrySet()
-                  .stream()
-                  .sorted((i1, i2)
-                              -> i2.getValue().compareTo(
-                                  i1.getValue())).map(e -> e.getValue())
-                  .collect(Collectors.toList());
+        Arrays.sort(freq, Collections.reverseOrder());
 
         int N = 8;
         int result = 0;
         int count = 1;
-        for(int i = 0;i<list.size();i++){
-            result += list.get(i)*count;
+        for(int i = 0;i<C;i++){
+            if(freq[i] == 0)
+                break;
+            result += freq[i]*count;
             N--;
             if(N == 0){
                 N = 8;
